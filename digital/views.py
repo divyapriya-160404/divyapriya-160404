9a34d23d1038f22ff0e7874832b594a1d704b960
divyapriya <divyapriya160404@gmail.com>
2025-05-22 16:01:54 +0530

    day8

--- digital/views.py
+++ digital/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
 # Create your views here.
 def homepage(request):
     return render(request,'index.html')
+
+
+def student_form(request):
+    if request.method =='POST':
+        name=request.POST.get('name')
+        rollno=request.POST.get('rollno')
+        email=request.POST.get('email')
+        student.objects.create(name=name,rollno=rollno,email=email)
+        return redirect('student_form')
+    students=students.object.all()
+    return render(request,'index.html',{'students':students}) 
--- divyapriya/settings.py
+++ divyapriya/settings.py
@@ -73,16 +73,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'divyapriya.wsgi.application'
 
-
-# Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'moon',
+'USER': 'root', # your MySQL username
+'PASSWORD': 'divyas123#', # your MySQL password
+'HOST': 'localhost',
+'PORT': '3307',
 }
+}
+
+
+ 
+
 
 
 # Password validation
