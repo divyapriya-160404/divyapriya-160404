4f2c87b877681d61169c97facee6e680848fe6be
divyapriya <divyapriya160404@gmail.com>
2025-05-24 15:31:46 +0530

    day10

--- digital/urls.py
+++ digital/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path ('home/',homepage),
-    path ('student_form/', student_form, name='student_form'),
-    # path ('signup_view', signup_view, name='signup'),
-    # path ('login_view', login_view, name='login'),
-    
-
+    path('home/',homepage),
+    path('student_form/',student_form, name='student_form'),
+    path('signup_view/',signup_view, name='signup'),
+    path('login_view/',login_view, name='login'),
+    path('student_biodata/',student_biodata, name='student_biodata')
 ]
--- digital/views.py
+++ digital/views.py
@@ -10,7 +10,7 @@ def homepage(request):
 
 
 def student_form(request):
-    if request.method =='POST':
+    if  request.method =='POST':
         name=request.POST.get('name')
         rollno=request.POST.get('rollno')
         email=request.POST.get('email')
@@ -21,41 +21,57 @@ def student_form(request):
     return render(request,'index.html',{'students':students}) 
 
 
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 
-    def signup_view(request):
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            email = request.POST.get('email')
-            password = request.POST.get('password')
-
-            if User.objects.filter(username=username).exists():
-                message.error(request,"Username already exists.")
-                return redirect('signup')
+        if User.objects.filter(username=username).exists():
+           message.error(request,"Username already exists.")
+           return redirect('signup')
             
-            user= User.objects.create_user(username=username, email=email, password=password)
-            messages.success(request, "Account created successfully")
-            return redirect('login')
+        user= User.objects.create_user(username=username, email=email, password=password)
+        messages.success(request, "Account created successfully")
+        return redirect('login')
 
-        return render(request, 'signup.html')
-
-        def login_view(request):
-            if request.method == 'POST':
-                username = request.POST.get('username')
-                password = request.POST.get('password')
-
-                user = authenticate(request, username=username, password=password)
-                if user:
-                    login(request, user)
-                    return redirect('student_form')
-                else:
-                    messages.error(request, "Invalid credentials.")
-                    return redirect('login')
-
-            return render(request, 'login.html') 
-
-        def logout_view(request):
-            logout(request)
-            return redirect('login')       
+    return render(request, 'signup.html')
 
 
+def login_view(request):
+     if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        user = authenticate(request, username=username, password=password)
+        if user:
+           login(request, user)
+           return redirect('student_form')
+        else:
+           messages.error(request, "Invalid credentials.")
+           return redirect('login')
+
+     return render(request, 'login.html') 
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')       
+
+def student_biodata(request):
+    if request.method == 'POST':
+        age=request.POST.get('age')
+        birth=request.POST.get('birth')
+        gender=request.POST.get('gender')
+        year=request.POST.get('year')
+        department=request.POST.get('department')
+        stream=request.POST.get('stream')
+        mobile_no=request.POST.get('mobile_no')
+        biodata.objects.create(age=age,birth=birth,gender=gender,year=year,department=department,stream=stream,mobile_no=mobile_no)
+        return redirect('student_biodata')
+
+    biodatas =biodata.objects.all()
+    return render(request,'biodata.html',{'biodatas':biodatas})
+
+
+    
+    
